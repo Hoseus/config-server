a24ea71f70542d1579c19a9b39c111265ad8cf47
hoseus <rjllopis@gmail.com>
2023-02-22 05:10:33 -0300

    feat: Initial version

---
+++ src/main/kotlin/com/hoseus/cloud/config/ConfigServer.kt
@@ -0,0 +1,12 @@
+package com.hoseus.cloud.config
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+
+@SpringBootApplication
+@EnableConfigServer
+class ConfigServer
+	fun main(args: Array<String>) {
+		runApplication<ConfigServer>(*args)
+	}
